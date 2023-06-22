c208d310f8d7671757264065e39af50844817f60
Thanh Nguyen Le Dang <127767615+LemuelJosh@users.noreply.github.com>
2023-06-22 13:14:14 +0700

    Update NoteSpawner.cs

--- Script/NoteSpawner.cs
+++ Script/NoteSpawner.cs
@@ -4,16 +4,46 @@ using UnityEngine;
 
 public class NoteSpawner : MonoBehaviour
 {
-    public GameObject noteSpawn;
+    public bool isTimer;
 
+    public List<GameObject> notesSpawn = new List<GameObject>();
+
+    public float timetoSpawn;
+    private float currentTimeToSpawn;
+
+    public bool isRandomized;
     void Start()
     {
-        
+        currentTimeToSpawn = timetoSpawn;
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+        if (isTimer)
+        {
+            UpdateTimer();
+
+        }
+    }
+    private void UpdateTimer()
+    {
+        if (currentTimeToSpawn > 0)
+        {
+            currentTimeToSpawn -= Time.deltaTime;
+        }
+        else
+        {
+            SpawnNote();
+            currentTimeToSpawn = timetoSpawn;
+        }
+    }
+    public void SpawnNote()
+    {
+        int index = isRandomized ? Random.Range(0, notesSpawn.Count) : 0;
+        if(notesSpawn.Count > 0)
+        {
+            Instantiate(notesSpawn[index], transform.position, transform.rotation);
+
+        }
     }
 }
